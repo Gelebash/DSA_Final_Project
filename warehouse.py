b8c8c73422153e525f670ebb08906789cc3c8c5c
vinnney <98568993+vinnney@users.noreply.github.com>
2024-12-04 00:08:01 -0500

    Update warehouse.py

--- warehouse.py
+++ warehouse.py
@@ -1,1 +1,15 @@
+from bin import Bin
+
+
+class Warehouse:
+    def __init__(self):
+        self.bins = []
+
+    def add_bin(self, capacity):
+        new_bin = Bin(capacity)
+        self.bins.append(new_bin)
+        return new_bin
+
+    def get_bins(self):
+        return self.bins
 
